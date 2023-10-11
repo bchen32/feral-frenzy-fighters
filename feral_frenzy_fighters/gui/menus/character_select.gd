16984ee5e2240e609fae1ec0077564ee571e44b3
CaptainJiggles11 <54753626+CaptainJiggles11@users.noreply.github.com>
2023-10-11 12:06:00 -0500

    Select Binding Fix

--- feral_frenzy_fighters/gui/menus/character_select.gd
+++ feral_frenzy_fighters/gui/menus/character_select.gd
@@ -78,7 +78,7 @@ func _process(delta):
 				p1_selection[p1_character].texture_normal = purplebox
 				on_character1_change()
 				
-		if Input.is_action_just_pressed("p1_attack"):
+		if Input.is_action_just_pressed("p1_jump"):
 			p1_locked = true
 			$Background/Player1Text/P1Ready.show()
 			on_locked_in()
@@ -108,7 +108,7 @@ func _process(delta):
 				p2_selection[p2_character].texture_normal = bluebox
 				on_character2_change()
 
-		if Input.is_action_just_pressed("p2_attack"):
+		if Input.is_action_just_pressed("p2_jump"):
 			p2_locked = true
 			$Background/Player2Text/P2Ready.show()
 			on_locked_in()
