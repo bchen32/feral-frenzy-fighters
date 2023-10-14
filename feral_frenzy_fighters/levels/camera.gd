46da3bd947a0ae2ecbffb2a9af60e73c726b8eb6
Benjamin Chen <benchen9708@gmail.com>
2023-10-13 21:40:27 -0500

    camera tweening

--- feral_frenzy_fighters/levels/camera.gd
+++ feral_frenzy_fighters/levels/camera.gd
@@ -2,10 +2,10 @@ extends Camera2D
 
 @export var players: Array[Node] = []
 @export var level: Node
-@export var pos_speed: float = 800.0
-@export var zoom_speed: float = 2.0
-@export var max_zoom: float = 2.0
-@export var margin: Vector2 = Vector2(800.0, 400.0)
+@export var pos_duration: float = 0.5
+@export var zoom_duration: float = 0.5
+@export var max_zoom: float = 1.5
+@export var margin: Vector2 = Vector2(800.0, 800.0)
 
 var event_spawner: Node
 var target_zoom: float
@@ -18,7 +18,7 @@ func _ready():
 	target_pos = position
 	target_zoom = zoom.x
 
-func _process(delta):
+func _process(_delta):
 	if event_spawner.set_camera_overview or len(players) == 1:
 		target_pos = initial_pos
 		target_zoom = 1.0
@@ -27,5 +27,6 @@ func _process(delta):
 		var zoom_x = (get_viewport().get_visible_rect().size.x - margin.x) / (abs(players[1].position.x - players[0].position.x))
 		var zoom_y = (get_viewport().get_visible_rect().size.y - margin.y) / (abs(players[1].position.y - players[0].position.y))
 		target_zoom = max(min(zoom_x, zoom_y, max_zoom), 1)
-	position = position.move_toward(target_pos, pos_speed * delta)
-	zoom = zoom.move_toward(Vector2(target_zoom, target_zoom), zoom_speed * delta)
+	var tween = create_tween().set_parallel(true)
+	tween.tween_property(self, "position", target_pos, pos_duration)
+	tween.tween_property(self, "zoom", Vector2(target_zoom, target_zoom), zoom_duration)
