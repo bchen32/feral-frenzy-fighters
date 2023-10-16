f723e373d1f5fb042220d644aaffa73877d1d3e9
Benjamin Chen <benchen9708@gmail.com>
2023-10-16 12:52:00 -0500

    improve behavior at edges

--- feral_frenzy_fighters/levels/camera.gd
+++ feral_frenzy_fighters/levels/camera.gd
@@ -36,9 +36,6 @@ func _process(delta):
 		var zoom_y = (viewport_rect.size.y - 2 * margin.y) / (abs(players[1].position.y - players[0].position.y))
 		var target_zoom = move_toward(old_target_zoom, max(min(zoom_x, zoom_y, max_zoom), min_zoom), target_zoom_speed * delta)
 		old_target_zoom = target_zoom
-		var target_pos = (players[0].position + players[1].position) / 2
-		target_pos = target_pos.clamp(viewport_rect.position + (viewport_rect.size / (2 * target_zoom)) - limit_margins,
-								viewport_rect.end - (viewport_rect.size / (2 * target_zoom)) + limit_margins)
 		var target_zoom_vect = Vector2(target_zoom, target_zoom)
 		var is_falling = players[0].velocity.y > 0 or players[1].velocity.y > 0
 		var fall_speed_mult = 1.0
@@ -47,13 +44,17 @@ func _process(delta):
 			var terminal = min(players[0].terminal_vel, players[1].terminal_vel)
 			fall_speed_mult = (fall_speed / terminal) + 1.0			
 		var zoom_dist = zoom.distance_to(target_zoom_vect)
-		var pos_dist = position.distance_to(target_pos)
 		var zoom_speed = min((zoom_in_speed_scale if target_zoom >= zoom.x else zoom_out_speed_scale) * zoom_dist, max_zoom_speed)
+		zoom = zoom.move_toward(target_zoom_vect, (fall_speed_mult if is_falling and target_zoom < zoom.x else 1.0) * zoom_speed * delta)
+		
+		var target_pos = (players[0].position + players[1].position) / 2
+		var clamped_pos = target_pos.clamp(viewport_rect.position + (viewport_rect.size / (2 * target_zoom)) - limit_margins,
+								viewport_rect.end - (viewport_rect.size / (2 * target_zoom)) + limit_margins)
+		var pos_dist = position.distance_to(clamped_pos)
 		var pos_diff = (target_pos - position).normalized()
 		var pos_speed = min(pos_speed_scale * pos_dist, max_pos_speed)
 		var pos_speed_x = abs(pos_speed * pos_diff.x)
 		var pos_speed_y = abs(pos_speed * pos_diff.y)
-		zoom = zoom.move_toward(target_zoom_vect, (fall_speed_mult if is_falling and target_zoom < zoom.x else 1.0) * zoom_speed * delta)
 		position.x = move_toward(position.x, target_pos.x, pos_speed_x * delta)
 		position.y = move_toward(position.y, target_pos.y, (fall_speed_mult if is_falling else 1.0) * pos_speed_y * delta)
 		position = position.clamp(viewport_rect.position + (viewport_rect.size / (2 * zoom)) - limit_margins,
