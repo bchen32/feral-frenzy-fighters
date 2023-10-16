40f1be217824ca8001887363c3b879a5afaf5e8a
CaptainJiggles11 <54753626+CaptainJiggles11@users.noreply.github.com>
2023-10-16 12:46:46 -0500

    Blood fix

--- feral_frenzy_fighters/player/state_machine/hit_state.gd
+++ feral_frenzy_fighters/player/state_machine/hit_state.gd
@@ -6,7 +6,11 @@ var hitstun: int
 
 func enter():
 	character.play_anim("hit")
-	character.play_particles(character.physics_blood, 0, 180)
+	character.play_particles(
+	character.physics_blood,
+	0,
+	180,
+	10 * character.percentage)
 	hitstun = floor(character.kb * character.kb_hitstun_scale)
 	character.set_physics_process(false)
 	
