8bcc23fa17e213815e29b8cfa163b8b8a0ebb2b5
NevPort <nevinportillo@gmail.com>
2023-10-30 18:25:25 -0500

    Finished 1st Framework of Water Level Event

--- feral_frenzy_fighters/levels/turtle_habitat/water_level.gd
+++ feral_frenzy_fighters/levels/turtle_habitat/water_level.gd
@@ -12,22 +12,41 @@ var high_tide_duration = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	water_level_process()
+	#water_level_process()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if event_spawner.start_external_event == true: # will only happen after volcano called event_spawner's _external_event()
+	if event_spawner.start_external_event == true: # will only happen after water_level called event_spawner's _external_event()
 		water_level_state = Water_Level_States.LOWTOHIGH
 		water_level_process()
 		event_spawner.start_external_event = false
 
 func water_level_process():
+	anim.stop()
+	
 	match water_level_state:
 		Water_Level_States.LOWTIDE:
-			pass
+			anim.play("LowTide")
+			await get_tree().create_timer(randi_range(min_high_tide_delay, max_high_tide_delay)).timeout
+			
+			event_spawner._external_event(false) # tell event_spawner water_level event wants to start next time
 		Water_Level_States.LOWTOHIGH:
-			pass
+			anim.play("Transition")
+			await anim.animation_finished
+			
+			water_level_state = Water_Level_States.HIGHTIDE
+			water_level_process()
 		Water_Level_States.HIGHTIDE:
-			pass
+			anim.play("HighTide")
+			await get_tree().create_timer(high_tide_duration).timeout
+			
+			water_level_state = Water_Level_States.HIGHTOLOW
+			water_level_process()
 		Water_Level_States.HIGHTOLOW:
-			pass
+			anim.play_backwards("Transition")
+			await anim.animation_finished
+			
+			event_spawner._external_event(true) # tell event_spawner water_level event has finished
+			water_level_state = Water_Level_States.LOWTIDE
+			water_level_process()
