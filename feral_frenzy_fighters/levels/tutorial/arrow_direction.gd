be22e5956168635e40a9320a549c3b5937e3e17c
NevPort <nevinportillo@gmail.com>
2023-11-06 13:44:41 -0600

    WIP on arrow direction

--- feral_frenzy_fighters/levels/tutorial/arrow_direction.gd
+++ feral_frenzy_fighters/levels/tutorial/arrow_direction.gd
@@ -4,8 +4,8 @@ class_name ArrowDirection
 
 signal on_direction_changed
 
-@export var right_position: Vector2 = Vector2(223, -87)
-@export var up_down_position: Vector2 = Vector2(0, 0)
+#@export var right_position: Vector2 = Vector2(223, -87)
+#@export var up_down_position: Vector2 = Vector2(0, 0)
 @export var target_area: GoalArea
 
 enum Direction {
@@ -44,24 +44,33 @@ func _process(delta):
 		if position_delta.x > 0:
 			# point LEFT
 			new_direction = Direction.LEFT
-			position = Vector2(right_position.x, right_position.y)
-			rotation_degrees = 0
+			#position = Vector2(right_position.x, right_position.y)
+			#rotation_degrees = 0
 		else:
 			# point RIGHT
 			new_direction = Direction.RIGHT
-			position = Vector2(-right_position.x, right_position.y)
-			rotation_degrees = 180
+			#position = Vector2(-right_position.x, right_position.y)
+			#rotation_degrees = 180
 	else:
 		if position_delta.y > 0:
 			# point UP
 			new_direction = Direction.UP
-			rotation_degrees = 90
-			position = Vector2(20, -150)
+			#rotation_degrees = 90
+			#position = Vector2(20, -150)
 		else:
 			# point DOWN
 			new_direction = Direction.DOWN
-			rotation_degrees = 270
-			position = Vector2(-40, -100)
+			#rotation_degrees = 270
+			#position = Vector2(-40, -100)
+	
+	if abs(global_position.angle_to_point(target_area.global_position)) < 0.1:
+		if rotation_degrees > global_position.angle_to_point(target_area.global_position):
+			rotation_degrees -= 1
+		elif rotation_degrees < global_position.angle_to_point(target_area.global_position):
+			rotation_degrees += 1
+	
+	print("arrow_direction: angle to target_area = ", global_position.angle_to_point(target_area.global_position))
+	print("arrow_direction: rotation_degrees = ", rotation_degrees)
 	
 	if current_direction != new_direction:
 		current_direction = new_direction
