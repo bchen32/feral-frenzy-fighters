9081c26c54e9b54bd8efd58835414ab55b7bffb5
jlq279 <joyce.l.qin@utexas.edu>
2023-11-14 15:43:46 -0600

    shows 4 hearts instead of icon and label

---
+++ feral_frenzy_fighters/gui/hud/lives_left.gd
@@ -0,0 +1,22 @@
+extends HBoxContainer
+
+@onready var heart = preload("res://gui/hud/heart.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func set_num_lives(num_lives):
+	await self.ready
+	var lives_left = get_child_count()
+	if num_lives > lives_left:
+		for i in range(lives_left, num_lives):
+			add_child(heart.instantiate())
+	elif num_lives < lives_left:
+		for i in range(num_lives, lives_left):
+			remove_child(get_child(0))
+	else:
+		pass
