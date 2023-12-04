f059665c7a92a86dbde5e225d59154ac37ffbb77
Bobby Youstra <bobbyyoustra@protonmail.com>
2023-12-03 22:14:53 -0600

    added intro cutscene

--- feral_frenzy_fighters/common/globals.gd
+++ feral_frenzy_fighters/common/globals.gd
@@ -2,7 +2,7 @@ extends Node
 
 enum States { AIR, AIR_ATTACK, AIR_JUMP, DASH, DASH_ATTACK, DASH_JUMP, GROUND_ATTACK, HIT, IDLE, WALK, WALK_JUMP }
 
-var cutscene_player_video_path: String = "res://gui/menus/cutscenes/intro_cutscene.ogv"
+var cutscene_player_video_path: String = "res://gui/menus/cutscenes/intro_cut.ogv"
 var cutscene_player_end_game: bool = false
 var player1_won: bool = false
 var player_sprites: Array[String] = ["cat", "cat"]
